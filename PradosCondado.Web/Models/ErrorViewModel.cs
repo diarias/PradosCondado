fc88b3d696ed25ce7f98a6eb8e59fa93dbbb28d1
Xime Moreno <xime0389@hotmail.com>
2021-04-21 18:48:57 -0500

    initial commit y rama xime

---
+++ PradosCondado.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PradosCondado.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
